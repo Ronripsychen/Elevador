cfef0646f6f009323ed873d176ebe05e9df599ff
ronripsychen <fabiosousadias@live.com>
2019-02-27 20:43:10 +0000

    corredor feito

---
+++ Assets/Scripts/charcontrol.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charcontrol : MonoBehaviour
+{
+    CharacterController cc;
+    public float speed = 10;
+    public float mousespeed = 10;
+    public float angle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 Direction = transform.forward * Input.GetAxisRaw("Vertical");
+        Direction += transform.right * Input.GetAxisRaw("Horizontal");
+
+        cc.Move(Direction * speed * Time.deltaTime);
+
+        angle += Input.GetAxis("Mouse X") * mousespeed * Time.deltaTime;
+
+        transform.eulerAngles = new Vector3(0, angle, 0);
+    }
+}
